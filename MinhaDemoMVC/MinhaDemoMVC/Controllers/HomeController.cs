640ad406818e16234c1170eab116e52ec6b4bfc7
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-05-07 15:36:03 -0300

    dummy application to goose around

---
+++ MinhaDemoMVC/MinhaDemoMVC/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MinhaDemoMVC.Models;
+
+namespace MinhaDemoMVC.Controllers
+{
+    [Route("")]
+    [Route("gestao-clientes")]
+    public class HomeController : Controller
+    {
+        [Route("")]
+        [Route("pagina-inicial")]
+        [Route("pagina-inicial/{id:int}/{categoria:guid}")]
+        public IActionResult Index(string id, Guid categoria)
+        {
+            return View();
+        }
+
+        [Route("privacidade")]
+        [Route("politica-de-privacidade")]
+        public IActionResult Privacy()
+        {
+            //return Json("{'nome':'Eduardo'}");
+
+            //var fileBytes = System.IO.File.ReadAllBytes(@"C:\Users\phili\Desktop\ASPNET-CORE.txt");
+            //var fileName = "arquivo.txt";
+            //return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+
+            return Content("Qualquer coisa");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [Route("erro-encontrado")]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MinhaDemoMVC/MinhaDemoMVC/Models/Filme.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.CodeAnalysis.Operations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhaDemoMVC.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo Titulo é obrigatório.")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "O titulo precisa ter entre 3 ou 60 caracteres.")]
+        public string Titulo { get; set; }
+
+        public DateTime DataLacamento { get; set; }
+
+        public string Genero { get; set; }
+
+        public decimal Valor { get; set; }
+
+        public int Avaliacao { get; set; }
+    }
+}
