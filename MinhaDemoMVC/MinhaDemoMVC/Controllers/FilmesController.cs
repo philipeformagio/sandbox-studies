c926ab6e6e3c20bce43285d1de7a2858988d3e3b
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-05-08 15:54:23 -0300

    dummy

---
+++ MinhaDemoMVC/MinhaDemoMVC/Controllers/FilmesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MinhaDemoMVC.Controllers
+{
+    public class FilmesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Adicionar()
+        {
+            return View();
+        }
+    }
+}
