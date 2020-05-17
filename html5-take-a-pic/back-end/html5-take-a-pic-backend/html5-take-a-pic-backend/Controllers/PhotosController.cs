53c6d20e224b2af2bf7886883768b8e00d92405f
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-05-17 09:19:09 -0300

    Posting the pic and save it locally

---
+++ html5-take-a-pic/back-end/html5-take-a-pic-backend/html5-take-a-pic-backend/Controllers/PhotosController.cs
@@ -0,0 +1,42 @@
+﻿using html5_take_a_pic_backend.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Drawing;
+using System.IO;
+
+namespace html5_take_a_pic_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotosController : ControllerBase
+    {
+        private IHostingEnvironment _env;
+        public PhotosController(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+
+
+        [HttpPost]
+        [DisableRequestSizeLimit]
+        public void Post([FromBody] Photo photo)
+        {
+            var photoName = DateTime.Now.ToString();
+
+            var webRoot = _env.ContentRootPath;
+            var PathWithFolderName = System.IO.Path.Combine(webRoot, "PhotosDirectory");
+
+            byte[] bytes = Convert.FromBase64String(photo.Base64Image);
+
+            Image image;
+            using (MemoryStream ms = new MemoryStream(bytes))
+            {
+                using (image = Image.FromStream(ms))
+                {
+                    image.Save(PathWithFolderName + "/ImageName.png");
+                }
+            }
+        }
+    }
+}
---
+++ html5-take-a-pic/back-end/html5-take-a-pic-backend/html5-take-a-pic-backend/Models/Photo.cs
@@ -0,0 +1,7 @@
+﻿namespace html5_take_a_pic_backend.Models
+{
+    public class Photo
+    {
+        public string Base64Image { get; set; }
+    }
+}
