06a3355585aabf0441d7341a9e9aa1ca6bef6642
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-05-20 18:22:43 -0300

    trying out filtering

--- AspNetCoreIdentity/AspNetCoreIdentity/Startup.cs
+++ AspNetCoreIdentity/AspNetCoreIdentity/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using AspNetCoreIdentity.Config;
+using AspNetCoreIdentity.Extensions;
 using KissLog;
 using KissLog.Apis.v1.Listeners;
 using KissLog.AspNetCore;
@@ -36,7 +37,10 @@ namespace AspNetCoreIdentity
             services.AddAuthorizantionConfig();
             services.ResolveDependencies();
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc(options =>
+            {
+                options.Filters.Add(typeof(AuditoriaFilter));
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
