cff36d73c5fab7c9f64b993cc1ecc4a967b44665
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-05-23 10:59:46 -0300

    FluentConfiguration

--- MinhaAppMvcCompleta/MinhaAppMvcCompleta/src/DevIO.Business/Models/Endereco.cs
+++ MinhaAppMvcCompleta/MinhaAppMvcCompleta/src/DevIO.Business/Models/Endereco.cs
@@ -5,6 +5,7 @@ namespace DevIO.Business.Models
     public class Endereco : Entity
     {
         public Guid FornecedorId { get; set; }
+
         public string Logradouro { get; set; }
         public string Numero { get; set; }
         public string Complemento { get; set; }
