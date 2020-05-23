ae0bd195dc34eacf6385c52b782ea11c21a669cc
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-05-23 16:19:51 -0300

    Repo

---
+++ MinhaAppMvcCompleta/MinhaAppMvcCompleta/src/DevIO.Data/Repository/Repository.cs
@@ -0,0 +1,66 @@
+﻿using DevIO.Business.Interfaces;
+using DevIO.Business.Models;
+using DevIO.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevIO.Data.Repository
+{
+    public abstract class Repository<TEntity> : IRepository<TEntity> where TEntity : Entity, new()
+    {
+        protected readonly MeuDbContext _db;
+        protected readonly DbSet<TEntity> _dbSet;
+        
+
+        public async Task<IEnumerable<TEntity>> Buscar(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _dbSet.AsNoTracking().Where(predicate).ToListAsync();
+        }
+
+        public virtual async Task<TEntity> ObterPorId(Guid id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public virtual async Task<List<TEntity>> ObterTodos()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public virtual async Task Adicionar(TEntity entity)
+        {
+            _dbSet.Add(entity);
+            await SaveChanges();
+        }
+
+        public virtual async Task Atualizar(TEntity entity)
+        {
+            _dbSet.Update(entity);
+            await SaveChanges();
+        }
+
+        public virtual async Task Remover(Guid id)
+        {
+            //_dbSet.Remove(await _dbSet.FindAsync(id));
+            _dbSet.Remove(new TEntity { Id = id });
+
+            await SaveChanges();
+        }
+
+        public async Task<int> SaveChanges()
+        {
+            return await _db.SaveChangesAsync();
+        }
+
+
+        public void Dispose()
+        {
+            _db?.Dispose();
+        }
+    }
+}
