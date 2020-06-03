485e2e8afab09acad46a60b49fa7d4ff89c3a29c
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-06-03 16:13:37 -0300

    business rules validation

---
+++ MinhaAppMvcCompleta/MinhaAppMvcCompleta/src/DevIO.Business/Services/BaseService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace DevIO.Business.Services
+{
+    public abstract class BaseService
+    {
+    }
+}
---
+++ MinhaAppMvcCompleta/MinhaAppMvcCompleta/src/DevIO.Business/Services/FornecedorService.cs
@@ -0,0 +1,32 @@
+﻿using DevIO.Business.Interfaces;
+using DevIO.Business.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace DevIO.Business.Services
+{
+    public class FornecedorService : BaseService, IFornecedorService
+    {
+        public Task Adicionar(Fornecedor fornecedor)
+        {
+            // validar o estado da entidade
+
+            // validar se nao existe fornecedor com o mesmo documento
+        }
+
+        public Task Atualizar(Fornecedor fornecedor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AtualizarEndereco(Endereco endereco)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Remover(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
