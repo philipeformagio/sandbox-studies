dc993d97a00c440d56ec376e8308b338f4545ddf
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-07-24 16:30:49 -0300

    Create UnitOfWork.cs

---
+++ Dapper.WebApi/Dapper.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using Dapper.Application.Interfaces;
+
+namespace Dapper.Infrastructure.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public UnitOfWork(IProductRepository productRepository)
+        {
+            Products = productRepository;
+        }
+
+        public IProductRepository Products { get; }
+    }
+}
