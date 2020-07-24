a1d308e0a53842efe3749adb0f96ec6604155d1d
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-07-24 16:19:53 -0300

    Create ProductRepository.cs

---
+++ Dapper.WebApi/Dapper.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,36 @@
+﻿using Dapper.Application.Interfaces;
+using Dapper.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Dapper.Infrastructure.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        public Task<int> AddAsync(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<Product>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateAsync(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
