de6c1f2c86949c2a78e42697d32da39f2e272d14
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-07-24 17:21:00 -0300

    removed unnecessary content.

--- Dapper.WebApi/Dapper.Infrastructure/ServiceRegistration.cs
+++ Dapper.WebApi/Dapper.Infrastructure/ServiceRegistration.cs
@@ -1,9 +1,6 @@
 ﻿using Dapper.Application.Interfaces;
 using Dapper.Infrastructure.Repositories;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Dapper.Infrastructure
 {
--- Dapper.WebApi/Dapper.WebApi/Controllers/ProductsController.cs
+++ Dapper.WebApi/Dapper.WebApi/Controllers/ProductsController.cs
@@ -5,16 +5,27 @@ using System.Threading.Tasks;
 
 namespace Dapper.WebApi.Controllers
 {
+    /// <summary>
+    /// ProductsController returns the product Entity
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
-    public class ProductController : ControllerBase
+    public class ProductsController : ControllerBase
     {
         private readonly IUnitOfWork unitOfWork;
-        public ProductController(IUnitOfWork unitOfWork)
+        /// <summary>
+        /// ProductsController initialize unitOfWork
+        /// </summary>
+        /// <param name="unitOfWork"></param>
+        public ProductsController(IUnitOfWork unitOfWork)
         {
             this.unitOfWork = unitOfWork;
         }
 
+        /// <summary>
+        /// Gets all products at once.
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
@@ -22,6 +33,11 @@ namespace Dapper.WebApi.Controllers
             return Ok(data);
         }
 
+        /// <summary>
+        /// Gets a product by a given Id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpGet("{id:int}")]
         public async Task<IActionResult> GetById(int id)
         {
@@ -30,6 +46,11 @@ namespace Dapper.WebApi.Controllers
             return Ok(data);
         }
 
+        /// <summary>
+        /// Adds a product into the database by a given product model.
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
         [HttpPost]
         public async Task<IActionResult> Add(Product product)
         {
@@ -37,6 +58,11 @@ namespace Dapper.WebApi.Controllers
             return Ok(data);
         }
 
+        /// <summary>
+        /// Deletes a product by a given id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpDelete]
         public async Task<IActionResult> Delete(int id)
         {
@@ -44,6 +70,11 @@ namespace Dapper.WebApi.Controllers
             return Ok(data);
         }
 
+        /// <summary>
+        /// Updates a product by a given id.
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
         [HttpPut]
         public async Task<IActionResult> Update(Product product)
         {
