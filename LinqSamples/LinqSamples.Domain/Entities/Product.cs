3566916e3d2c0bb28e4f89ef13c21046f0de8d36
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-07-26 12:18:50 -0300

    projtect created to learn a bit more about linq

---
+++ LinqSamples/LinqSamples.Domain/Entities/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinqSamples.Domain.Entities
+{
+    public class Product
+    {
+        private List<Product> _products;
+        public Product()
+        {
+            _products = new List<Product>();
+        }
+
+        public List<Product> GetProductList()
+        {
+            _products.Add(new Product() { Id = Guid.NewGuid(), Name = "Banana", Quantity = 5, Price = 2, ValidUntil = DateTime.Now.AddDays(5) });
+            _products.Add(new Product() { Id = Guid.NewGuid(), Name = "Morango", Quantity = 5, Price = 25, ValidUntil = DateTime.Now.AddDays(2) });
+            _products.Add(new Product() { Id = Guid.NewGuid(), Name = "Acabaxi", Quantity = 4, Price = 1, ValidUntil = DateTime.Now.AddDays(4) });
+            _products.Add(new Product() { Id = Guid.NewGuid(), Name = "Pera", Quantity = 12, Price = 12, ValidUntil = DateTime.Now.AddDays(3) });
+            _products.Add(new Product() { Id = Guid.NewGuid(), Name = "Melao", Quantity = 5, Price = 35, ValidUntil = DateTime.Now.AddDays(10) });
+
+            return _products;
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public DateTime ValidUntil { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ LinqSamples/LinqSamples/Program.cs
@@ -0,0 +1,49 @@
+﻿using LinqSamples.Domain.Entities;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+
+namespace LinqSamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var product = new Product();
+
+            //var products = product.GetProductList().Where(x => x.Price > 4);
+
+            //foreach (var prod in products)
+            //{
+            //    Console.WriteLine(prod.Name);
+            //}
+
+            //new Product().GetProductList().Where(x => x.Price > 4).ToList().ForEach(x =>
+            //{
+            //    Console.WriteLine(x.Name);
+            //    Console.WriteLine(x.Price);
+            //});
+
+            var products = new Product().GetProductList();
+
+            var obj = products.Where(p => p.Name.StartsWith("A") || p.Name.EndsWith("o"))
+                               //.Select(p => new { p.Name, p.Price })
+                               .Select(p => new SelectedProduct { Name = p.Name, Price = p.Price }) //same result as previous line but now it is typed
+                               .ToList();
+
+            obj.ForEach(item =>
+            {
+                Console.WriteLine(JsonConvert.SerializeObject(item));
+                Console.WriteLine(" ----------------------- ");
+            });
+
+            Console.ReadKey();
+        }
+    }
+
+    public class SelectedProduct
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
