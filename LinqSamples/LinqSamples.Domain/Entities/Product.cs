4ef76cac4415a9b70c603cd7aba1b3048ed58761
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-07-30 18:45:19 -0300

    studies

--- LinqSamples/LinqSamples.Domain/Entities/Product.cs
+++ LinqSamples/LinqSamples.Domain/Entities/Product.cs
@@ -11,13 +11,31 @@ namespace LinqSamples.Domain.Entities
             _products = new List<Product>();
         }
 
-        public List<Product> GetProductList()
+        public List<Product> GetFruits()
         {
-            _products.Add(new Product() { Id = Guid.NewGuid(), Name = "Banana", Quantity = 5, Price = 2, ValidUntil = DateTime.Now.AddDays(5) });
-            _products.Add(new Product() { Id = Guid.NewGuid(), Name = "Morango", Quantity = 5, Price = 25, ValidUntil = DateTime.Now.AddDays(2) });
-            _products.Add(new Product() { Id = Guid.NewGuid(), Name = "Acabaxi", Quantity = 4, Price = 1, ValidUntil = DateTime.Now.AddDays(4) });
-            _products.Add(new Product() { Id = Guid.NewGuid(), Name = "Pera", Quantity = 12, Price = 12, ValidUntil = DateTime.Now.AddDays(3) });
-            _products.Add(new Product() { Id = Guid.NewGuid(), Name = "Melao", Quantity = 5, Price = 35, ValidUntil = DateTime.Now.AddYears(1) });
+            _products.Add(new Product() { Id = Guid.NewGuid(), Name = "Banana", Quantity = 5, Price = 2, ValidUntil = DateTime.Now.AddDays(5), Category = "Frutas" });
+            _products.Add(new Product() { Id = Guid.NewGuid(), Name = "Morango", Quantity = 5, Price = 25, ValidUntil = DateTime.Now.AddDays(2), Category = "Frutas" });
+            _products.Add(new Product() { Id = Guid.NewGuid(), Name = "Acabaxi", Quantity = 4, Price = 4, ValidUntil = DateTime.Now.AddDays(4), Category = "Frutas" });
+            _products.Add(new Product() { Id = Guid.NewGuid(), Name = "Pera", Quantity = 12, Price = 12, ValidUntil = DateTime.Now.AddDays(3), Category = "Frutas" });
+            _products.Add(new Product() { Id = Guid.NewGuid(), Name = "Melao", Quantity = 45, Price = 5, ValidUntil = DateTime.Now.AddYears(1), Category = "Frutas" });
+
+            return _products;
+        }
+
+        public List<Product> GetEletronics()
+        {
+            _products.Add(new Product() { Name = "IPhone", Price = 8000, Category = "Eletronicos" });
+            _products.Add(new Product() { Name = "Galaxy S7 Edge", Price = 2000, Category = "Eletronicos" });
+            _products.Add(new Product() { Name = "Pen Drive", Price = 100, Category = "Eletronicos" });
+
+            return _products;
+        }
+
+        public List<Product> GetCloths()
+        {
+            _products.Add(new Product() { Name = "Jeans", Price = 40, Category = "Cloths" });
+            _products.Add(new Product() { Name = "Shoes", Price = 100, Category = "Cloths" });
+            _products.Add(new Product() { Name = "T-Shirt", Price = 90, Category = "Cloths" });
 
             return _products;
         }
@@ -27,5 +45,6 @@ namespace LinqSamples.Domain.Entities
         public int Quantity { get; set; }
         public DateTime ValidUntil { get; set; }
         public decimal Price { get; set; }
+        public string Category { get; set; }
     }
 }
--- LinqSamples/LinqSamples/Program.cs
+++ LinqSamples/LinqSamples/Program.cs
@@ -1,6 +1,7 @@
 ﻿using LinqSamples.Domain.Entities;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace LinqSamples
@@ -37,17 +38,100 @@ namespace LinqSamples
             //    Console.WriteLine(JsonConvert.SerializeObject(item));
             //});
 
-            var produtos = new Product().GetProductList();
+            //var produtos = new Product().GetProductList();
 
-            var obj = produtos
-                        //.Where(p => p.ValidUntil.Year == 2021)
-                        .Select(p => new SelectedProduct { Name = p.Name, Price = p.Price, ExpiredDay = p.ValidUntil.Day })
-                        .ToList();
+            //var obj = produtos
+            //            //.Where(p => p.ValidUntil.Year == 2021)
+            //            .Select(p => new SelectedProduct { Name = p.Name, Price = p.Price, ExpiredDay = p.ValidUntil.Day })
+            //            .ToList();
 
-            obj.ForEach(item =>
+            //obj.ForEach(item =>
+            //{
+            //    Console.WriteLine(JsonConvert.SerializeObject(item));
+            //});
+
+            //var produtos = new Product().GetProductList();
+            //List<Product> products = new List<Product>();
+
+            ////var produto1 = produtos.First();
+            ////var produto2 = produtos.FirstOrDefault();
+
+            ////var produto1 = produtos.Last();
+            ////var produto2 = produtos.LastOrDefault();
+
+            //if (produtos.Any(p => p.Quantity > 10))
+            //{
+            //    Console.WriteLine("Has items with quantity greater than 10.");
+            //}
+
+            //produtos.ForEach(item =>
+            //{
+            //    Console.WriteLine(JsonConvert.SerializeObject(item));
+            //});
+
+
+            //var products1 = new Product().GetProductList();
+
+            //var products2 = new List<Product>();
+            //products2.Add(new Product() { Name = "Galaxy", Price = 2500 });
+            //products2.Add(new Product() { Name = "IPhoneX", Price = 7900 });
+
+            //products1.AddRange(products2);
+
+            //products1.ForEach(x =>
+            //{
+            //    Console.WriteLine(JsonConvert.SerializeObject(x.Name));
+            //});
+
+            //int[] pairNumbers = { 2, 4, 6, 8, 10 };
+            //int[] oddNumbers = { 1, 3, 5, 7, 9 };
+            //int[] mixedNumbers = { 1, 2, 3, 4 };
+
+            //var newListSameNumbers = mixedNumbers.Intersect(pairNumbers); //returns a new list with found same same numbers
+
+            //var itemsThatDoestNotExistInTheOtherList = mixedNumbers.Except(pairNumbers); // returns a new list of not found items
+
+            //Range
+            //var sequence = Enumerable.Range(10, 3); // return a list starting at 10 and finishing at 12
+            //var repeat = Enumerable.Repeat("Philipe", 10); // returns a new list of the first argument times the second argument
+
+
+            //var products = new Product().GetFruits();
+            //var pricestProduct = products.Max(p => p.Price); // pricest product in the list
+            //var biggerQuantity = products.Max(p => p.Quantity); // bigger quantity product
+            //var averagePrice = products.Average(p => p.Price); // average price of all product's price in the list
+            //var minPrice = products.Min(p => p.Price); // minimum price in the list
+            //var sum = products.Sum(p => p.Price); // total sum of all prices
+
+
+            var fruitsList = new Product().GetFruits();
+            var eletronicsList = new Product().GetEletronics();
+            var clothsList = new Product().GetCloths();
+
+            List<Product> allProducts = new List<Product>();
+            allProducts.AddRange(fruitsList);
+            allProducts.AddRange(eletronicsList);
+            allProducts.AddRange(clothsList);
+
+            //allProducts.ForEach(x =>
+            //{
+            //    Console.WriteLine(JsonConvert.SerializeObject(x));
+            //});
+
+            Console.WriteLine("----------------------------------");
+            var result = from p in allProducts
+                         group p by p.Category into grouped
+                         select new
+                         {
+                             CategoryName = grouped.Key,
+                             MinPrice = grouped.Min(x => x.Price),
+                             MaxPrice = grouped.Max(x => x.Price)
+                         };
+
+            foreach (var item in result)
             {
                 Console.WriteLine(JsonConvert.SerializeObject(item));
-            });
+            }
 
 
             Console.ReadKey();
