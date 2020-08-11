cc23d183123ecee5f1d0fe6d3a97db5a29a43e52
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-08-11 19:36:22 -0300

    studies

---
+++ MyFirstAPI/Model/Fornecedor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyFirstAPI.Model
+{
+    public class Fornecedor
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [StringLength(100, ErrorMessage = "The field {0} must have between {2} and {1} characters", MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [StringLength(14, ErrorMessage = "The field {0} must have between {2} and {1} characters", MinimumLength = 11)]
+        public string Document { get; set; }
+        public int TipoFornecedor { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
