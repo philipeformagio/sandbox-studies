d62703bd3c3e30a1bdab6131eae08bae6dcb4e6f
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-09-13 14:56:19 -0300

    asdf

---
+++ the-js-junkyard/basics-assignment-1-start/basics-assignment-1-start/assignment.js
@@ -0,0 +1,11 @@
+const userInput = 99;
+let result;
+result = 18 + userInput;
+
+result = result - 2;
+result = result * 2;
+result = result / 2;
+
+
+console.log('result', result);
+console.log('userInput', userInput);
