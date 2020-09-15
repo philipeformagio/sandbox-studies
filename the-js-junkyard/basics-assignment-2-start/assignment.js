5ae3b3bc56f45334df00b8b5370aa66154d28bea
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-09-15 10:50:06 -0300

    js junk yard

---
+++ the-js-junkyard/basics-assignment-2-start/assignment.js
@@ -0,0 +1,22 @@
+const task3Element = document.getElementById('task-3');
+
+function greeting() {
+    alert('Hello, there!');
+}
+
+function greetingName(name) {
+    alert(`Hello, ${name}`);
+}
+
+function concatenatingString(param1, param2, param3) {
+    return `${param1}, ${param2}, ${param3}`
+}
+
+greeting();
+
+greetingName('Zlatan');
+
+task3Element.addEventListener('click', greeting);
+
+const result = concatenatingString('Zlatan', 'Philipe', 'Laylay');
+alert(result);
