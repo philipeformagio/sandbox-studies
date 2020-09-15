2f84733e1bf23cd949d3879c42134b71aa16a28c
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-09-15 10:25:21 -0300

    Update app.js

--- the-js-junkyard/basics-01-starting-project/assets/scripts/app.js
+++ the-js-junkyard/basics-01-starting-project/assets/scripts/app.js
@@ -1,12 +1,10 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-currentResult = (currentResult + 10) * 3 / 2 - 1;
+function add() {
+    currentResult = currentResult + userInput.value;
+    outputResult(currentResult, '');
+}
 
-let calculcationDescription = `(${defaultResult} + 10) * 3 / 2 -1`;
-let errorMessage = 'An error ' + 
-                   ' occurred!';
-let errorMessage2 = 'An error \n occurred!';
+addBtn.addEventListener('click', add);
 
-
-outputResult(currentResult, calculcationDescription);
