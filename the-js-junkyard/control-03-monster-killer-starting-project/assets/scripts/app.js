01dcdc04f9c81a696887114d118349e047990300
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-10-03 18:44:36 -0300

    Update app.js

--- the-js-junkyard/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ the-js-junkyard/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
@@ -7,16 +8,33 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler() {
-    const monsterDamageTaken = dealMonsterDamage(ATTACK_VALUE);
+function attackMonster(attackMode) {
+    // let maxDamage;
+    // if(attackMode === 'ATTACK') {
+    //     maxDamage = ATTACK_VALUE;
+    // } else if(attackMode === 'STRONG_ATTACK') {
+    //     maxDamage = STRONG_ATTACK_VALUE;
+    // }
+    const monsterDamageTaken = dealMonsterDamage(attackMode);
     currentMonsterHealth -= monsterDamageTaken;
     const playerDamageTaken = dealPlayerDamage(MONSTER_ATTACK_VALUE);
     currentPlayerHealth -= playerDamageTaken;
-    if (currentMonsterHealth <= 0) {
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
         alert('You won!');
-    } else if (currentPlayerHealth <= 0) {
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
         alert('You lost!');
+    } else if (currentPlayerHealth <=0 && currentMonsterHealth <= 0) {
+        alert('draw!');
     }
 }
 
+function attackHandler() {
+    attackMonster(ATTACK_VALUE);
+}
+
+function strongAttackHandler() {
+    attackMonster(STRONG_ATTACK_VALUE);
+}
+
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
