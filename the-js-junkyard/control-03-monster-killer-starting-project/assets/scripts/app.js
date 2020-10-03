261cd433db8d94a5120f27e35230098f08e41a25
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-10-03 19:09:36 -0300

    Update app.js

--- the-js-junkyard/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ the-js-junkyard/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,6 +1,7 @@
 const ATTACK_VALUE = 10;
 const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
+const HEAL_VALUE = 20;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -36,5 +37,11 @@ function strongAttackHandler() {
     attackMonster(STRONG_ATTACK_VALUE);
 }
 
+function healPlayerHandler() {
+    increasePlayerHealth(HEAL_VALUE);
+    
+}
+
 attackBtn.addEventListener('click', attackHandler);
 strongAttackBtn.addEventListener('click', strongAttackHandler);
+healBtn.addEventListener('click', healPlayerHandler)
