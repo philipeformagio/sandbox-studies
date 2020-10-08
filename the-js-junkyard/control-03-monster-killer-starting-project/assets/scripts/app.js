a01d1b547abd761535fba7137e5abd0db109dc3a
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-10-08 20:46:22 -0300

    Update app.js

--- the-js-junkyard/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ the-js-junkyard/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -4,12 +4,11 @@ const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
 
 const enteredValue = prompt('Maximum life for you and the monster.', '100');
-
+let chosenMaxLife = parseInt(enteredValue);
 if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
     chosenMaxLife = 100;
 }
 
-let chosenMaxLife = parseInt(enteredValue);
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 let hasBonusLife = true;
