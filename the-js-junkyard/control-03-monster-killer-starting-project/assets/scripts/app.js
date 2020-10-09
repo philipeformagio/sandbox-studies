87b06a43a9885669e4828747efe20ee736317966
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-10-09 10:00:23 -0300

    Update app.js

--- the-js-junkyard/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ the-js-junkyard/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -2,6 +2,13 @@ const ATTACK_VALUE = 10;
 const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
+const LOG_EVENT_PLAYER_ATTACK = 'PLAYER_ATTACK';
+const LOG_EVENT_PLAYER_STRONG_ATTACK = 'PLAYER_STRONG_ATTACK';
+const LOG_EVENT_MONSTER_ATTACK = 'MONSTER_ATTACK';
+const LOG_EVENT_PLAYER_HEAL = 'PLAYER_HEAL';
+const LOG_EVENT_GAME_OVER = 'GAME_OVER';
+
+const battleLog = [];
 
 const enteredValue = prompt('Maximum life for you and the monster.', '100');
 let chosenMaxLife = parseInt(enteredValue);
@@ -14,6 +21,61 @@ let currentPlayerHealth = chosenMaxLife;
 let hasBonusLife = true;
 
 adjustHealthBars(chosenMaxLife);
+
+function writeToLog(event, value, monsterHealth, playerHealth) {
+    if (
+        event !== 'PLAYER_ATTACK' &&
+        event !== 'PLAYER_STRONG_ATTACK' &&
+        event !== 'MONSTER_ATTACK' &&
+        event !== 'PLAYER_HEAL' &&
+        event !== 'GAME_OVER'
+    ) {
+        return;
+    }
+
+    let logEntry;
+    if (event === LOG_EVENT_PLAYER_ATTACK) {
+        logEntry = {
+            event: event,
+            value: value,
+            target: 'MONSTER',
+            finalMonsterHealth: monsterHealth,
+            finalPlayerHealth: playerHealth
+        };
+    } else if (event === LOG_EVENT_PLAYER_STRONG_ATTACK) {
+        logEntry = {
+            event: event,
+            value: value,
+            target: 'MONSTER',
+            finalMonsterHealth: monsterHealth,
+            finalPlayerHealth: playerHealth
+        };
+    } else if (event === LOG_EVENT_MONSTER_ATTACK) {
+        logEntry = {
+            event: event,
+            value: value,
+            target: 'PLAYER',
+            finalMonsterHealth: monsterHealth,
+            finalPlayerHealth: playerHealth
+        };
+    } else if (event === LOG_EVENT_PLAYER_HEAL) {
+        logEntry = {
+            event: event,
+            value: value,
+            target: 'PLAYER',
+            finalMonsterHealth: monsterHealth,
+            finalPlayerHealth: playerHealth
+        };
+    } else if (event === LOG_EVENT_GAME_OVER) {
+        logEntry = {
+            event: event,
+            value: value,            
+            finalMonsterHealth: monsterHealth,
+            finalPlayerHealth: playerHealth
+        };
+    }
+    battleLog.push(logEntry);
+}
 
 function reset() {
     currentPlayerHealth = chosenMaxLife;
