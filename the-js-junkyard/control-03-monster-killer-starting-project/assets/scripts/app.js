6342f1477466dfb3879be9dad5f9a63e6061fea7
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-10-21 20:21:20 -0300

    Update app.js

--- the-js-junkyard/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ the-js-junkyard/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -20,7 +20,15 @@ function getMaxLifeValues() {
     return parsedValue;
 }
 
-let chosenMaxLife = getMaxLifeValues();
+let chosenMaxLife;
+
+try {
+    chosenMaxLife = getMaxLifeValues();
+} catch (error) {
+    console.log(error);
+    chosenMaxLife = 100;
+    alert('You entered something wrong, default value of 100 was used.');
+}
 
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
