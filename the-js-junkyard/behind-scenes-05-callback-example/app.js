2c1669513a1f8c5f896f6c883dfc96727d84a34f
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-11-02 15:43:03 -0300

    .

---
+++ the-js-junkyard/behind-scenes-05-callback-example/app.js
@@ -0,0 +1,16 @@
+const addListenerBtn = document.getElementById('add-listener-btn');
+const clickableBtn = document.getElementById('clickable-btn');
+const messageInput = document.getElementById('click-message-input');
+
+let person = {name: 'Philipe'};
+
+function printMessage() {
+  const value = messageInput.value;
+  console.log(value || 'Clicked me!');
+}
+
+function addListener() {
+  clickableBtn.addEventListener('click', printMessage);
+}
+
+addListenerBtn.addEventListener('click', addListener);
