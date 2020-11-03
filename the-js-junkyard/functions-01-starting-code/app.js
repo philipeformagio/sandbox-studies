82e7ab9de514793651e43ee4d45eeeb812dd4d93
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-11-02 21:19:14 -0300

    Update app.js

--- the-js-junkyard/functions-01-starting-code/app.js
+++ the-js-junkyard/functions-01-starting-code/app.js
@@ -4,4 +4,15 @@ function startGame() {
     console.log('Game is starting...');
 }
 
+// const person = {
+//     name: 'Max',
+//     greet: function greet() {
+//         console.log('Hello there!');
+//     }
+// }
+
+// person.greet();
+
+console.log(typeof(startGame));
+
 startGameBtn.addEventListener('click', startGame);
