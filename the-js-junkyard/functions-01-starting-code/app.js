9b4d1383b484ed31903e9231e669521021765ccd
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-11-04 11:02:59 -0300

    Update app.js

--- the-js-junkyard/functions-01-starting-code/app.js
+++ the-js-junkyard/functions-01-starting-code/app.js
@@ -1,18 +1,38 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-function startGame() {
-    console.log('Game is starting...');
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK;
+
+let isGamingRunning = false;
+
+const getRandomChoice = function() {
+    const choices = ['ROCK', 'PAPER', 'SCISSORS'];
+    const random = Math.floor(Math.random() * choices.length);
+    return choices[random];
 }
 
-// const person = {
-//     name: 'Max',
-//     greet: function greet() {
-//         console.log('Hello there!');
-//     }
-// }
+const getPlayerChoice = function() {
+    const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '').toUpperCase();
+    if (
+        selection !== ROCK &&
+        selection !== PAPER &&
+        selection !== SCISSORS
+    ) {
+        const defaultChoice = getRandomChoice();
+        alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
+        return DEFAULT_USER_CHOICE;
+    }
+    return selection;
+};
 
-// person.greet();
-
-console.log(typeof(startGame));
-
-startGameBtn.addEventListener('click', startGame);
+startGameBtn.addEventListener('click', function startGame() {
+    if(isGamingRunning) {
+        return;
+    }
+    isGamingRunning = true;
+    console.log('Game is starting...');
+    const playerSelection = getPlayerChoice();
+    console.log(playerSelection);
+});
