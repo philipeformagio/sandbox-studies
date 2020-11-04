4b15819bd26b525e61089a7b84a6f8ed97b1cc23
Philipe Formagio Grosso <philipe.formagio@gmail.com>
2020-11-04 18:40:14 -0300

    ,

--- the-js-junkyard/functions-01-starting-code/app.js
+++ the-js-junkyard/functions-01-starting-code/app.js
@@ -1,19 +1,23 @@
 const startGameBtn = document.getElementById('start-game-btn');
+const resetGameBtn = document.getElementById('reset-game-btn');
 
 const ROCK = 'ROCK';
 const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
 const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER_WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER_WINS';
 
 let isGamingRunning = false;
 
-const getRandomChoice = function() {
+const getRandomChoice = () => {
     const choices = ['ROCK', 'PAPER', 'SCISSORS'];
     const random = Math.floor(Math.random() * choices.length);
     return choices[random];
 };
 
-const getPlayerChoice = function() {
+const getPlayerChoice = () => {
     const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`, '').toUpperCase();
     if (
         selection !== ROCK &&
@@ -21,10 +25,32 @@ const getPlayerChoice = function() {
         selection !== SCISSORS
     ) {
         const defaultChoice = getRandomChoice();
-        alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
-        return DEFAULT_USER_CHOICE;
+        alert(`Invalid choice! We chose ${defaultChoice} for you!`);
+        return defaultChoice;
     }
     return selection;
+};
+
+const getWinner = (cChoice, pChoice) => {
+    return cChoice === pChoice
+        ? RESULT_DRAW
+        : (cChoice === ROCK && pChoice === PAPER) || 
+          (cChoice === PAPER && pChoice === SCISSORS) ||
+          (cChoice === SCISSORS && pChoice === ROCK)
+        ? RESULT_PLAYER_WINS
+        : RESULT_COMPUTER_WINS
+
+    // if (cChoice === pChoice) {
+    //     return RESULT_DRAW;
+    // } else if (
+    //     (cChoice === ROCK && pChoice === PAPER) || 
+    //     (cChoice === PAPER && pChoice === SCISSORS) ||
+    //     (cChoice === SCISSORS && pChoice === ROCK)
+    // ) {
+    //     return RESULT_PLAYER_WINS;
+    // } else {
+    //     return RESULT_COMPUTER_WINS;
+    // }
 };
 
 startGameBtn.addEventListener('click', function startGame() {
@@ -33,6 +59,12 @@ startGameBtn.addEventListener('click', function startGame() {
     }
     isGamingRunning = true;
     console.log('Game is starting...');
-    const playerSelection = getPlayerChoice();
-    console.log(playerSelection);
+    const playerChoice = getPlayerChoice(); console.log(playerChoice);
+    const computerChoice = getRandomChoice(); console.log(computerChoice);
+    const winner = getWinner(computerChoice, playerChoice);
+    console.log(winner);
 });
+
+resetGameBtn.addEventListener('click', function resetGame() {
+    isGamingRunning = false;
+})
